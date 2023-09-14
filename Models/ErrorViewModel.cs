d5bd39ca5c360f5852a8b2c59ac3c2c58b16e0f5
Simon Sjöö <simon.sjoo21@gmail.com>
2023-09-14 14:18:32 +0200

    Default MVC files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace eventhantering.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
